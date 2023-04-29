2c0ba884888d7dd76af800e2f52f5a45d9abd2a4
ahmed <elgrhy@icloud.com>
2023-04-29 21:48:45 +0400

    assets

---
+++ lib/demo.dart
@@ -0,0 +1,55 @@
+// demo data
+
+List<String> demoBigImages = [
+  'assets/images/bruch_mix.png',
+  'assets/images/canberries_mix.png',
+  'assets/images/dried_mango.png',
+  'assets/images/dried_pineapple.png',
+  'assets/images/japanese_mix.png',
+  'assets/images/pecan_nuts.png',
+];
+
+List<Map<String, dynamic>> demoMediumCardData = [
+  {
+    'name': 'Bruch Mix',
+    'image': 'assets/images/bruch_mix.png',
+    'rating': 4.3,
+    'deliveryTime': 30,
+
+  },
+  {
+    'name': 'Canberries Mix',
+    'image': 'assets/images/canberries_mix.png',
+    'rating': 4.3,
+    'deliveryTime': 30,
+
+  },
+  {
+    'name': 'Dried Mango',
+    'image': 'assets/images/dried_mango.png',
+    'rating': 4.3,
+    'deliveryTime': 30,
+
+  },
+  {
+    'name': 'Dried Pineapple',
+    'image': 'assets/images/dried_pineapple.png',
+    'rating': 4.3,
+    'deliveryTime': 30,
+
+  },
+  {
+    'name': 'Japanese Mix',
+    'image': 'assets/images/japanese_mix.png',
+    'rating': 4.3,
+    'deliveryTime': 30,
+
+  },
+  {
+    'name': 'Pecan Nuts',
+    'image': 'assets/images/pecan_nuts.png',
+    'rating': 4.3,
+    'deliveryTime': 30,
+
+  },
+];
